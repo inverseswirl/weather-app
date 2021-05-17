d7f8350b04bb0b02cbe512bb3cdf988f26625582
inverseswirl <68209878+inverseswirl@users.noreply.github.com>
2021-05-17 13:56:19 +1000

    update

--- src/components/error.js
+++ src/components/error.js
@@ -3,34 +3,30 @@ import Neutral from '../style/neutral';
 import Title from './title';
 import {BiErrorCircle} from 'react-icons/bi';
 
-class Error extends React.Component {
-    constructor(props) {
-      super(props);
-   
-    }
-    
+const Error =()=>{
+  
 
 
-  render() {
-
- 
  return(
      <div>
        <Neutral/>
        <Title/>
         <div className="search">             
-              <input className="search-input" 
-                      type="search"
-                      placeholder="Enter place name here!"
-                    />
-              <button  className="search-btn"></button>
+          <input className="search-input" 
+                  type="search"
+                  placeholder="Enter place name here!"
+                />
+          <button  className="search-btn"></button>
         </div>
-     <div className="error">
-         <h1><BiErrorCircle size={20}/> Please try again with the right name! </h1>
+        <div className="error">
+            <h1><BiErrorCircle size={20}/> Please try again with the right name! </h1>
+        </div>
      </div>
-     </div>
+ 
+
+ 
  )
 }
-}
+
 
 export default Error;
