35824efd3cf7bed0e4b04a093efacf026b251c2a
inverseswirl <68209878+inverseswirl@users.noreply.github.com>
2021-06-24 02:59:33 +1000

    css-updated

--- src/components/data.js
+++ src/components/data.js
@@ -20,21 +20,19 @@ const Data=({current,location})=>{
 <div className="data">
        
     <p className="data-temperature">Current temperature is {temperature}<sup>o</sup>C </p>
-    <div className="data-flex">
-       <p className="data-flex-feelslike">It feels like {feelslike}<sup>o</sup>C</p>
-       <div className="data-flex-location">
+    
+    <div className="data-group">
+       <p className="data-group-feelslike">It feels like {feelslike}<sup>o</sup>C</p>
+       <div className="data-group-location">
           <h1><span className="pin-shadow">< FiMapPin  style={{ animation: 'pin 2s linear 1'}} color="#FFB362" size={30}/></span> {name}</h1>
           <h2>{country}</h2>
        </div>
-      
     </div>
+      
      
 
     
-   
- 
-
-        <p className="data-other"> UV index is <span>
+      <p className="data-other"> UV index is <span>
         {current.uv_index>=0 && current.uv_index<=2 ? "low" : null}
         {current.uv_index>=3 && current.uv_index<=5 ? "moderate" : null}
         {current.uv_index>=6 && current.uv_index<=7 ? "high" : null}
@@ -47,22 +45,23 @@ const Data=({current,location})=>{
 
 
    
-   <div className="data-weathertype">
+
         
-    <p className="data-weathertype-weather"> {weather_descriptions} {" "}
-            {current && current.weather_descriptions[0]==="Sunny" ?  <IoIosSunny color="#ffc966" size={50} style={{paddingLeft:'10px',marginBottom:'-10px'}}/>: null}
-            {current && current.weather_descriptions[0]==="Cloudy" ?     <HiCloud color="#c5e2f7" style={{paddingLeft:'10px',marginBottom:'-8px'}} size={38}/>: null}
-            {current && current.weather_descriptions[0]==="Partly cloudy" ?  <IoPartlySunnyOutline color="#dedea0" size={40} style={{paddingLeft:'10px',marginBottom:'-3px'}}/> : null}
-            {current && current.weather_descriptions[0]==="Overcast" ? <BsCloudFill color="grey" size={40}  style={{paddingLeft:'10px',marginBottom:'-8px'}}/> : null}
-            {current && current.weather_descriptions[0]==="Thunderstorm" ?   <RiThunderstormsLine color="grey" size={45}  style={{paddingLeft:'10px',marginBottom:'-8px'}}/> : null}
-            {current && current.weather_descriptions[0]==="Haze" ?    <RiHazeLine color="rgb(100, 93, 93)" size={36}  style={{paddingLeft:'10px',marginBottom:'-4px'}}/>: null}
-            {current && current.weather_descriptions[0]==="Light Rain" ?    <GiRaining color="rgb(105, 93, 93)" size={38}  style={{paddingLeft:'10px',marginBottom:'-5px'}}/>  : null}
-            {current && current.weather_descriptions[0]==="Clear" ?   <FaRegSmileBeam color="lightyellow" size={36} style={{paddingLeft:'10px',marginBottom:'-5px'}} />  : null} 
-            {current && current.weather_descriptions[0]==="Smoke" ?   <WiDust color="lightyellow" size={45} style={{paddingLeft:'5px',marginBottom:'-13px'}} />  : null} 
-            {current && current.temperature <0  ?  <GiSnowing color="#DADADA" style={{paddingLeft:'5px',marginBottom:'-11px'}} size={32}/> : null}
-    </p>  
+        <p className="data-weathertype"> {weather_descriptions} {" "}
+                {current && current.weather_descriptions[0]==="Sunny" ?  <IoIosSunny color="#ffc966" size={50} style={{paddingLeft:'10px',marginBottom:'-10px'}}/>: null}
+                {current && current.weather_descriptions[0]==="Cloudy" ?     <HiCloud color="#c5e2f7" style={{paddingLeft:'10px',marginBottom:'-8px'}} size={38}/>: null}
+                {current && current.weather_descriptions[0]==="Partly cloudy" ?  <IoPartlySunnyOutline color="#dedea0" size={40} style={{paddingLeft:'10px',marginBottom:'-3px'}}/> : null}
+                {current && current.weather_descriptions[0]==="Overcast" ? <BsCloudFill color="grey" size={40}  style={{paddingLeft:'10px',marginBottom:'-8px'}}/> : null}
+                {current && current.weather_descriptions[0]==="Thunderstorm" ?   <RiThunderstormsLine color="grey" size={45}  style={{paddingLeft:'10px',marginBottom:'-8px'}}/> : null}
+                {current && current.weather_descriptions[0]==="Haze" ?    <RiHazeLine color="rgb(100, 93, 93)" size={36}  style={{paddingLeft:'10px',marginBottom:'-4px'}}/>: null}
+                {current && current.weather_descriptions[0]==="Light Rain" ?    <GiRaining color="rgb(105, 93, 93)" size={38}  style={{paddingLeft:'10px',marginBottom:'-5px'}}/>  : null}
+                {current && current.weather_descriptions[0]==="Clear" ?   <FaRegSmileBeam color="lightyellow" size={36} style={{paddingLeft:'10px',marginBottom:'-5px'}} />  : null} 
+                {current && current.weather_descriptions[0]==="Smoke" ?   <WiDust color="lightyellow" size={45} style={{paddingLeft:'5px',marginBottom:'-13px'}} />  : null} 
+                {current && current.temperature <0  ?  <GiSnowing color="#DADADA" style={{paddingLeft:'5px',marginBottom:'-11px'}} size={32}/> : null}
+                {current && current.temperature >23 && current.weather_descriptions[0]==="Rain"  ?  <GiRaining color="rgb(105, 93, 93)" size={38}  style={{paddingLeft:'10px',marginBottom:'-5px'}}/>  : null }
+        </p>  
            
-     </div>
+
      
 
 
